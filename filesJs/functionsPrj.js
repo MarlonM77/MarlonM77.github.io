ba94a96aec23aaf5f4304ffdbf936192499caea8
Marlon <marlondeep77@gmail.com>
2021-11-13 00:16:01 -0500

    deployment project

---
+++ filesJs/functionsPrj.js
@@ -0,0 +1,14 @@
+var indexValue = 1;
+showImg(indexValue);
+function side_slide(e){showImg(indexValue += e);}
+
+function showImg(e){
+    var i;
+    const img = document.querySelectorAll(".slid");
+    if(e > img.length){indexValue = 1;}
+    if(e < 1){indexValue = img.length;}
+    for(i = 0; i < img.length; i++){
+        img[i].style.display = "none";  
+    }
+    img[indexValue-1].style.display = "block";
+}
